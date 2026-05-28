e4078213f5f1a59f7b29b082782cb3b3b6e0c9bd
kdawson133 <kdawson133@startmail.com>
2026-05-28 10:34:03 +1000

    added

---
+++ install-formula.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#  NOTE: update the homebrew database
+brew update
+
+# NOTE: list of forlulae to install
+PACKAGES=(
+  neovim
+  stow
+  fzf
+  zoxide
+  starship
+  bat
+  eza
+  ripgrep
+  fd
+  node
+)
+
+brew install ${PACKAGES[@]}
+
