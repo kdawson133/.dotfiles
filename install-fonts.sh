bbd354e68b9646a05a88ca706fe9f4aa431a3067
kdawson133 <kdawson133@startmail.com>
2026-05-28 10:54:33 +1000

    added

--- install-fonts.sh
+++ install-fonts.sh
@@ -2,7 +2,9 @@
 # NOTE: Install Fonts
 OS=$(uname)
 if [[ $OS == 'Darwin' ]]; then
-  sudo cp -R fonts/* /Library/Fonts/
+  find . -type f -name "*.ttf" -exec cp {} ~/Library/Fonts \;
+  find . -type f -name "*.otf" -exec cp {} ~/Library/Fonts \;
 elif [[ $OS == 'Linux' ]]; then
-  sudo cp -R fonts/* /usr/share/fonts/
+  find . -type f -name "*.ttf" -exec cp {} ~/.local/share/fonts \;
+  find . -type f -name "*.otf" -exec cp {} ~/.local/share/fonts \;
 fi
